97874c20738a5aa646cf02c73923c6d725651e48
Marco Puccio <mpuccio90@gmail.com>
2016-04-02 20:50:14 -0300

    fix url slash

--- htpl/urls.py
+++ htpl/urls.py
@@ -9,5 +9,5 @@ from .views import IndexTemplateView, GenericTemplateNameView
 app_name = 'htpl'
 urlpatterns = [
     url(r'^$', IndexTemplateView.as_view(), name='index'),
-    url(r'^(?P<tpl_name>[\w\-\_]+)$', GenericTemplateNameView.as_view(), name="no_index_template"),
+    url(r'^(?P<tpl_name>[\w\-\_]+)/$', GenericTemplateNameView.as_view(), name="no_index_template"),
 ]
